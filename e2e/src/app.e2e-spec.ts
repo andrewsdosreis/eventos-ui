a9822a84c77b6bb82485b3c627d4f8912c32bc02
Andrews Reis <andrewsdosreis@gmail.com>
2019-06-18 01:54:57 -0300

    fix: ajustando nome do projeto

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to helpdesk!');
+    expect(page.getParagraphText()).toEqual('Welcome to eventos!');
   });
 });
